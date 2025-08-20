573e9e7c0f90e9ce89baf3a19d086b7fe76e401e
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-08-20 10:29:40 +0530

    Update googleSheets.js

--- server/services/googleSheets.js
+++ server/services/googleSheets.js
@@ -1,20 +1,63 @@
 const { google } = require("googleapis");
-require("dotenv").config();
 
-const SPREADSHEET_ID = process.env.GOOGLE_SHEET_ID;
+// 🔑 Your Service Account JSON
+const creds = {
+  type: "service_account",
+  project_id: "bold-bond-469518-n4",
+  private_key_id: "233f3bd094b9c0c58b9f6be20d6d017c1ae225f6",
+  private_key: `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDalrh6ITeuadmc
+BJFeMuTqa5C2v7P0qVX/dIuAsqNitwDjOyM5geBY4JKIyzQpJ38/xjdiCdSoVQin
+eWYhR09XFubNChYFXCGV/MgSY+GDTjQ6JLAgSZLejq9+1M1zJwiQ4FzcmcYS3CN7
+aHFuUEnnQrbsN2sXK0CKQgkPihqJRbyA4AyPcnA2xHlJPUSgKlekqPDgkXEvh9j5
+lCXL0emLQ/BnccavZ/jJV1SizrSVS55r+wJPObMj6+8SrvZg6qrrxkedDKCphvMg
+X6+ltVd9a1RhS36GcbKRCj+XhzkJ6ZP/JA5sjj8IRpFzKlSe2nq+TgjiiAAg7NgJ
+6e8wUjyVAgMBAAECggEAGkj5BoCjkjbaytLWyzHKCrFkHcGoKWHKfFU+qDty/74b
+XnfASNxIBs/8dvQegdtkxx22D7U4bASLrZ3gzNwnG4UevGW716moRUG4KlP17IT1
+LU+3mS5XuakFS/ZZfUEim89LA2D/a0mq/55D05zWgjdoyTdxyn+yO13s+oAkKEDw
+Ssc+58xAn4TRN4OuRY+LH/fhl3gsqNUVg4TJAVHGYF9KbzIubLVAUIGw0tkora/x
+sXvbnDoauvqgNceFtLynhTKN++vqcxztnkxbxc6tSgrDlck2Kdwmbd+xr4g7m5zB
+t9AK4NRA10nT6zlu8NC0x3+TRs5gIz34CLu/zDkTqQKBgQD6GsLTgQPL4OZgd4mL
+cuJY85x0VIduvBhU1FF9YUOJKndAaj8uZ8AutfAFO7suf/ZR3EnahNnUR4TAs52O
+E5uNCrifVOyWolCisgaLb95CkabipA9DW5FHv4t953FlrqkX8rhZxkDqGqEMd8Om
+NHsz1i2GFVR6VP1Vt6aUgiagzQKBgQDfvcea0PNgARHHoVxZBhx7/8QV2l/P/IkU
+AbaDFqYAnCBlvy53JkvqRoiWb61RxcJWEsndX+JegUXzpq4WYGG/BZqRZ88C8AOn
+2MqnvdyA4h4pXNx4I1aV4BnVlc7kSzvDfLfkJU2PzfoiVXn84ZWgzNhQX6jXWkDr
++lGx6ulq6QKBgQCQyLsuW4qKFRjokWrDmm8kFWrmASUh2quvN/YwwgSpvDaqyJbo
+01Dy+YoITYrPoW5D92WdpbjAZeKY96Hm5LzwylsUicIodbB+btbRE6K0IMNegqD/
+WlvhECa9bSU9Ov5SicVk2xsrKt0+bXqKlekYmg1iiKiN5PN/EsDRSgL4pQKBgGyZ
+LwNEHH0ix8B/39UOFFFTiZeZ0Rug1eAyHKZwHc75+Pep3iHTItHpvm5WcmfYd0VU
+BUtAOR3p3UwwQRt3WLphO+8L1fz1BrE3ltXoX9jm6ADno9NkQ6inh6KV/j/tDGsM
+ZC2GtajxL1Hu7zEBRaExDGWmqMsucYiYjYKwJO/RAoGAfGc6WXTK2BVqG6yt4bDB
+vUcQaTUoY3jLED9kPnw1M/YGcbC40GUpbzuseKR23Lmx8xWT6DCa6Xybm3MSrnKO
+/UbVKmSJmLiJjJ6x2ar28ZD5B42GznxDJHrRxaAFQT7bgT3CRH91ioJTXZLfqaHz
+sd24GrwTDNGRCYfYoZ+u+eo=
+-----END PRIVATE KEY-----\n`,
+  client_email: "sheet-service@bold-bond-469518-n4.iam.gserviceaccount.com",
+  client_id: "116636218015051192562",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/sheet-service%40bold-bond-469518-n4.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+};
+
+// ✅ Your Google Sheet ID
+const SPREADSHEET_ID = "1BwRHdA3CpqUY02xExJaiEHCyEMPK8cSOBA-_M74BL-4";
 const MAX_ROWS_PER_SHEET = 50000;
 
-// ✅ Auth with env credentials (no file needed)
+// Auth with direct JSON
 const auth = new google.auth.GoogleAuth({
- credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS.replace(/\n/g, "\\n")),
+  credentials: creds,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
-
 
 async function getSheetsClient() {
   const authClient = await auth.getClient();
   return google.sheets({ version: "v4", auth: authClient });
 }
+
+
 
 async function getSheetList() {
   const sheets = await getSheetsClient();
